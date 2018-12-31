a812c4ccdc21cac6968492acb589970f90010aa4
Adriano Magalhães <adrianodmagalhaes@gmail.com>
2018-12-31 13:43:54 -0200

    boolean e structs

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/depois/Booleanos.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/5 - Booleanos/depois/Booleanos.cs
@@ -10,6 +10,26 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
+            //bool possuiSaldo = 1;
+
+            bool possuiSaldo = true;
+
+            Console.WriteLine($"Possui saldo: {possuiSaldo}");
+
+            int dias = DateTime.Now.Day;
+
+            Console.WriteLine($"Dias: {dias}");
+
+            bool diasPar = (dias % 2 == 0);
+
+            if (diasPar)
+            {
+                Console.WriteLine("dias é um numero par");
+            }
+            else
+            {
+                Console.WriteLine("dias é um numero impar");
+            }
         }
     }
 }
--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
@@ -18,6 +18,45 @@ namespace certificacao_csharp_roteiro
             Console.WriteLine($"Longitude1 = {Longitude1}");
             Console.WriteLine($"Latitude2 = {Latitude2}");
             Console.WriteLine($"Longitude2 = {Longitude2}");
+
+            PosicaoGPS posicao1;
+            posicao1.Latitude = 13.78;
+            posicao1.Longitude = 29.51;
+
+            PosicaoGPS posicao2 = new PosicaoGPS() { Latitude = 40.23, Longitude = 17.4 };
+
+            PosicaoGPS posicao = new PosicaoGPS(40.23, 17.4);
+
+            Console.WriteLine(posicao1);
+
         }
+    }
+
+    interface IGPS
+    {
+        bool EstaNoHemisferioNorte();
+    }
+
+    struct PosicaoGPS : IGPS
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public PosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}";
+        }
+
     }
 }
