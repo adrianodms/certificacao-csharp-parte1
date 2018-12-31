251876da26bbe662b2d26e85c5bd92cdbbce2002
Adriano Magalhães <adrianodmagalhaes@gmail.com>
2018-12-31 10:02:22 -0200

    float double e decimal

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/depois/Inteiros.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/depois/Inteiros.cs
@@ -10,6 +10,51 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
+            int idade = 15;
+
+            char resposta = 'S'; // System.Char
+
+            byte nivelDeAzul = 0xFF; // = 255 em decimal
+
+            //nivelDeAzul = -3;
+
+            short passageirosVoo = 230; // System.Int16
+
+            passageirosVoo = -7;
+
+            int populacao = 1500; // System.Int32
+
+            populacao = -2300;
+
+            long populacaoDoBrasil = 207_660_992; // +/- 207 milhoes
+
+            sbyte nivelDeBrilho = -127; // System.Sbyte
+
+            ushort passageirosNavio = 230; // System.UInt16
+
+            uint estoque = 1500; // System.UInt132
+
+            ulong populacaoDoMundo = 7_000_000_000; // 7 bilhoes - System.UInt64
+
+
+            Console.WriteLine($"resposta: {resposta}");
+            Console.WriteLine($"nivelDeAzul: {nivelDeAzul}");
+            Console.WriteLine($"passageirosVoo: {passageirosVoo}");
+            Console.WriteLine($"populacao: {populacao}");
+            Console.WriteLine($"populacaoDoBrasil: {populacaoDoBrasil}");
+            Console.WriteLine($"nivelDeBrilho: {nivelDeBrilho}");
+            Console.WriteLine($"passageirosNavio: {passageirosNavio}");
+            Console.WriteLine($"estoque: {estoque}");
+            Console.WriteLine($"populacaoDoMundo: {populacaoDoMundo}");
+
+            //nivelDeAzul = 0xFFFF; // = 65535 em descimal
+            //passageirosVoo = 230000; // System.Int16
+            //populacao = 100055505050540540; //
+
+
+
+
+
         }
     }
 }
